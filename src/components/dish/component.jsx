573014a736846eb1b45fe36720dec1c9ef3000aa
lyudmila shmadchenko <lvshmadchenko@appewa.com>
2024-05-21 19:42:18 +0300

    Add counter

--- src/components/dish/component.jsx
+++ src/components/dish/component.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react'
+
 export const Dish = ({ dish }) => {
+  const [count, setCount] = useState(0)
   return (
     <>
-      <div>{ dish.name }</div>
-      <span>{ dish.price }</span>
+      <div>{ dish.name }:{ dish.price }</div>
+      <div>
+        <button onClick={ () => count && setCount(count - 1) }>-</button>
+        { count }
+        <button onClick={ () => count < 5 && setCount(count + 1) }>+</button>
+      </div>
     </>
   )
 }
