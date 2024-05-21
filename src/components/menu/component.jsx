743e403b40492179157680cecf122b83d60ae13d
lyudmila shmadchenko <lvshmadchenko@appewa.com>
2024-05-21 19:51:41 +0300

    Add conditional rendering

--- src/components/menu/component.jsx
+++ src/components/menu/component.jsx
@@ -1,15 +1,21 @@
 import { Dish } from '@/components'
 
 export const Menu = ({ menu }) => {
-  return (
-    <ul>
-      {
-        menu.map(dish => {
+  if (menu && menu.length) {
+    return (
+      <ul>
+        { menu.map(dish => {
           return <li key={ dish.id }>
             <Dish dish={ dish }/>
           </li>
-        })
-      }
-    </ul>
+        }) }
+      </ul>
+    )
+  }
+
+  return (
+    <div>
+      There is no menu yet, but it’ll be coming soon :)
+    </div>
   )
 }
