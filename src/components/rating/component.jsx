ecb6f6852db942dddd633f5d7550f99fe82e193b
lyudmila shmadchenko <lvshmadchenko@appewa.com>
2024-05-28 23:30:39 +0300

    Move review form

--- src/components/rating/component.jsx
+++ src/components/rating/component.jsx
@@ -21,6 +21,3 @@ export const Rating = ({ value, onChange }) => {
     </div>
   )
 }
-
-
-
--- src/components/restaurants/component.jsx
+++ src/components/restaurants/component.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Restaurant, ReviewForm } from '@/components'
+import { Restaurant } from '@/components'
 
 export const Restaurants = ({ restaurants, tabIndex }) => {
   const [activeTab, setActiveTab] = useState(Math.min(tabIndex, restaurants.length))
@@ -21,13 +21,6 @@ export const Restaurants = ({ restaurants, tabIndex }) => {
       </ul>
 
       <Restaurant restaurant={ restaurants[activeTab] }/>
-
-      <div>
-        <h3> Leave a review </h3>
-        <ReviewForm/>
-      </div>
-
     </>
   )
 }
-
