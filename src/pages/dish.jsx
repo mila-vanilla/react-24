c89dc1aaad517ebff71168731bd1f9d9a34e13cb
lyudmila shmadchenko <lvshmadchenko@appewa.com>
2024-06-14 17:00:17 +0300

    Handle no dish id case

--- src/pages/dish.jsx
+++ src/pages/dish.jsx
@@ -1,6 +1,15 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate, useParams } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export const DishPage = () => {
+  const { dishId } = useParams()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!dishId)
+      navigate('/restaurants', { replace: true })
+  }, [dishId])
+
   return (
     <Outlet/>
   )
